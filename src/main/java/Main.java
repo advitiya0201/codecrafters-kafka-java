d9dde3a01504a1b51826ce9ccb67b95a9fbd54f0
advitiya0201 <f20210979@pilani.bits-pilani.ac.in>
2025-04-09 23:48:32 +0530

    commit correlation Id

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,34 +3,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    System.err.println("Logs from your program will appear here!");
+    public static void main(String[] args) throws IOException {
+        System.err.println("Logs from your program will appear here!");
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 9092;
-    try {
-       serverSocket = new ServerSocket(port);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       // Wait for connection from client.
-       clientSocket = serverSocket.accept();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          String messageSize = "23"; //4 bytes
-          String correlationId = "4";
-          String response = messageSize + correlationId;
-          clientSocket.getOutputStream().write(response.getBytes());
-          clientSocket.close();
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    int messageSize = 0x00000000; //4 bytes
+                    int correlationId = 0x00000007;
+                    String response = String.valueOf(messageSize + correlationId);
+                    clientSocket.getOutputStream().write(response.getBytes());
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
+        System.out.println("args[0] is: " + args[0]);
     }
-    System.out.println("args[0] is: "+args[0]);
-  }
 }
